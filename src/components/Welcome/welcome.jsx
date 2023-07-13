896aa66a502d79ef2554da24d5961cba5455dc85
miche <michel99stolle@gmail.com>
2023-07-13 10:46:29 +0200

    Commit before QR-Scanner experiment

--- src/components/Welcome/welcome.jsx
+++ src/components/Welcome/welcome.jsx
@@ -1,15 +1,16 @@
 import Header from "../Header/Header.jsx";
 import './Welcome.css'
-import Character from "./welcomeAssets/SRH_Nurse.svg";
+import Character from "./welcomeAssets/SRH_Nurse_welcome.png";
 function Welcome() {
 
     return (
         <>
-            <div className={'gridCon'}></div>
-            <div className={'welcomeHeader'}></div>
-            <Header></Header>
-            <img src={Character} alt={'NurseCharacter'} className={'nurseWelcome'}/>
-
+            <div className={'gridCon'}>
+                <div className={'welcomeHeader'}></div>
+                <Header></Header>
+                <button className={'fo'}></button>
+                <img src={Character} alt={'NurseCharacter'} className={'nurseWelcome'}/>
+            </div>
         </>
     )
 }
